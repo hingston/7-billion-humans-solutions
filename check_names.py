1b244a4c8c6c08c6a90edf41c0e9d23db420f88c
hingston <26564456+hingston@users.noreply.github.com>
2019-05-18 13:47:23 +0930

    Update check_names.py

--- check_names.py
+++ check_names.py
@@ -1,64 +1,73 @@
 #!/usr/bin/env python3
 
 import glob
-import re
 import os
+import re
+
 
 def get_files(directories, recursive=True):
-	files = []
-	for d in directories:
-		files_fullname = glob.glob(d + os.sep + '**', recursive=recursive)
-		for f in files_fullname:
-			files.append(dict(
-				dir = os.path.dirname(f), 
-				file = '' if os.path.isdir(f) else os.path.basename(f)
-			))
-	return files
-	
+    files = []
+    for d in directories:
+        files_fullname = glob.glob(d + os.sep + '**', recursive=recursive)
+        for f in files_fullname:
+            files.append(dict(
+                dir=os.path.dirname(f),
+                file='' if os.path.isdir(f) else os.path.basename(f)
+            ))
+    return files
+
+
 def check_year(min_year, max_year):
-	def check(f):	
-		regex_result = re.search('Year (.*) - ', f['file'])
-		try:
-			year = int(regex_result.group(1))
-			return (year < min_year or year > max_year)
-		except (ValueError, AttributeError):
-			return False
-	return check
-		
+    def check(f):
+        regex_result = re.search('Year (.*) - ', f['file'])
+        try:
+            year = int(regex_result.group(1))
+            return (year < min_year or year > max_year)
+        except (ValueError, AttributeError):
+            return False
+
+    return check
+
+
 def check_solution_type(guilty_solutions):
-	def check(f):
-		try:
-			type = re.search('\((.*)\)', f['file']).group(1)
-			return (type not in guilty_solutions)
-		except AttributeError:
-			return False
-	return check
-	
+    def check(f):
+        try:
+            type = re.search('\((.*)\)', f['file']).group(1)
+            return (type not in guilty_solutions)
+        except AttributeError:
+            return False
+
+    return check
+
+
 def check_files(files, solution_types, min_year, max_year):
-	only_files = list(filter(lambda f: f['file'] != '', files))	
-	txt_files = list(filter(lambda f: f['file'].endswith('.txt'), only_files))
-	return {
-		'Wrong extension (.txt):' : list(filter(lambda f: not f['file'].endswith('.txt'), only_files)),
-		'File must start with "Year ":' : list(filter(lambda f: not f['file'].startswith('Year '), txt_files)),
-		'Year must be between 2 and 68:' : list(filter(check_year(min_year, max_year), txt_files)),
-		'File name must specify speed, size or both between brackets:' : 
-			list(filter(check_solution_type(solution_types), txt_files))
-	}
+    only_files = list(filter(lambda f: f['file'] != '', files))
+    txt_files = list(filter(lambda f: f['file'].endswith('.txt'), only_files))
+    return {
+        'Wrong extension (.txt):': list(filter(lambda f: not f['file'].endswith('.txt'), only_files)),
+        'File must start with "Year ":': list(filter(lambda f: not f['file'].startswith('Year '), txt_files)),
+        'Year must be between 2 and 68:': list(filter(check_year(min_year, max_year), txt_files)),
+        'File name must specify speed, size or both between brackets:':
+            list(filter(check_solution_type(solution_types), txt_files))
+    }
+
 
 def print_results(results):
-	results_with_issues = dict(filter(lambda r: len(r[1]) > 0, results.items()))
-	if len(results_with_issues) == 0:
-		print('Finished! There are no issues :)')
-	else:
-		for r in results_with_issues:
-			print(r)
-			for v in results_with_issues[r]:
-				print('- "{}{}{}"'.format(v['dir'], os.sep, v['file']))
-	
+    results_with_issues = dict(filter(lambda r: len(r[1]) > 0, results.items()))
+    if len(results_with_issues) == 0:
+        print('Finished! There are no issues :)')
+    else:
+        for r in results_with_issues:
+            print(r)
+            for v in results_with_issues[r]:
+                print('- "{}{}{}"'.format(v['dir'], os.sep, v['file']))
+
+
 def main():
-	print_results(
-		check_files(get_files(['Solutions99+', 'Solutions50+']), ['speed', 'size', 'both'], 2, 68)
-	)
-	
+    print_results(
+        check_files(get_files(['Solutions99+', 'Solutions50+']), ['speed', 'size', 'both'], 2, 68)
+    )
+
+
 if __name__ == "__main__":
-	main()
+    main()
